d232ab46f883bccb8e8a80899e03c57dc2579dd3
Paul Pound <ppound@upei.ca>
2012-05-29 16:35:43 -0300

    added alternate template for collection view

---
+++ islandora_basic_collection/islandora-basic-collection-grid.tpl.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * islandora-basic-collection.tpl.php
+ * 
+ *
+ * 
+ * This file is part of Islandora.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with the program.  If not, see <http ://www.gnu.org/licenses/>.
+ */
+?>
+<?php drupal_set_title('grid_view'); ?>
+
+<div class="islandora-basic-collection-wrapper">
+  <div class="islandora-basic-collection clearfix">
+  <?php print($variables['pager']); ?>
+  <?php foreach($associated_objects_array as $key => $value): ?>
+    <dl class="islandora-basic-collection-object <?php print $value['class']; ?>">
+        <dt class="islandora-basic-collection-thumb"><?php print $value['thumb_link']; ?></dt>
+        <dd class="islandora-basic-collection-caption"><?php print $value['title_link']; ?></dd>
+    </dl>
+  <?php endforeach; ?>
+  <?php print($variables['pager']); ?>
+</div>
+</div>
