3ff6e9da4cf28577d2e1b2da28a4a73d010ca3e9
Paul Pound <ppound@upei.ca>
2012-06-01 16:00:29 -0300

    running change view links through theme links

--- islandora_basic_collection/theme/islandora-basic-collection-wrapper.tpl.php
+++ islandora_basic_collection/theme/islandora-basic-collection-wrapper.tpl.php
@@ -25,7 +25,9 @@
 
 <div class="islandora-basic-collection-wrapper">
   <div class="islandora-basic-collection clearfix">
-    <span class="islandora-basic-collection-display-switch"><?php print $grid_link; ?> <?php print $list_link; ?></span>
+    <span class="islandora-basic-collection-display-switch">
+     <?php print theme('links', array('links' => $view_links, 'attributes' => array('class' => array('links', 'inline'))));?>
+    </span>
     <?php print $collection_pager; ?>
     <?php print $collection_content; ?>
     <?php print $collection_pager; ?>
