74bcd0664c2f21a9cfff72c7193aa073bc859ae4
Paul Pound <ppound@upei.ca>
2012-06-01 09:53:56 -0300

    updated islandora api documentation

--- islandora.api.php
+++ islandora.api.php
@@ -2,13 +2,13 @@
 
 function hook_islandora_purge_datastream ($object_id, $datastream_id){}
 
-function hook_islandora_purge_object($object_id) {} 
+function hook_islandora_purge_object($object) {} 
 
-function hook_islandora_view_object($object_id){}
+function hook_islandora_view_object($object){}
 
 function hook_islandora_get_types(){}
 
-function hook_islandora_add_datastream($object_id) {}
+function hook_islandora_add_datastream($object) {}
 
 /**
  * creates and populates a php Fedora object. 
@@ -26,3 +26,19 @@ function hook_islandora_datastream_edit($object, $dsid){}
  *     );
  */
 function hook_islandora_ingest_registry($collection_pid) {}
+
+
+/**
+ * alter an object before it gets used further down the stack
+ * @param type $object 
+ *   a tuque FedoraObject
+ */
+function hook_islandora_object_alter ($fedora_object){}
+
+/**
+ * insert or remove rendered elements by implementing this function 
+ * in your module 
+ * @param type $arr 
+ *   an arr of rendered views
+ */
+function hook_islandora_display_alter ($arr){}
