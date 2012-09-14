59537aa751ba8be5e35dfaa148bcced218d8e5b5
William Panting <willtp87@hotmail.com>
2012-09-14 11:14:41 -0300

    ISLANDORA-731 Starting API documentation.

---
+++ fedora_repository.api.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ *   This file defines the hooks that fedora_repository (islandora)
+ *   makes available.
+ */
+
+/**
+ * Implements hook_islandora_tabs().
+ * This hook lets one add tabs to the object page in Islandora.
+ *
+ * @param array $content_models
+ *   An Array of content model objects.  A content model is only included
+ *   if the object actualy exists with a ISLANDORACM datastream.
+ * @param string $pid
+ *   The Fedora PID of the object who's page is firing the hook.
+ *
+ * @return array
+ *   $tabset a tab definition.
+ */
+function hook_islandora_tabs($content_models, $pid) {
+
+  $tabset['A TAB'] = array(
+    '#type' => 'tabpage',
+    '#title' => t('A TITLE'),
+    '#content' => 'content')
+  );
+
+  return $tabset;
+}
