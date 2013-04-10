010fd655a9edbb1d6090680f072c2807ab911612
jonathangreen <jonathan@discoverygarden.ca>
2013-04-10 20:57:44 -0300

    Added dependancy

--- tests/travis_setup.sh
+++ tests/travis_setup.sh
@@ -13,6 +13,7 @@ export CATALINA_HOME='.'
 cd $HOME
 pyrus channel-discover pear.drush.org
 pyrus channel-discover pear.phpqatools.org
+pyrus channel-discover pear.netpirates.net
 pyrus install drush/drush
 pyrus install pear/PHP_CodeSniffer
 pyrus install pear.phpunit.de/phpcpd
