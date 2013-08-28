81cc6be97748b359e8cf4543cca10b18c83e6ce0
MorganDawe <morgandawe@hotmail.com>
2013-08-28 09:58:21 -0300

    Removed console log function and reference to old view object permission.

--- js/add_print.js
+++ js/add_print.js
@@ -10,7 +10,6 @@
   $(document).ready(function() {
     $('.tabs .primary').append('<img id="print_btn" title="Print" src="' + Drupal.settings.basePath + Drupal.settings.islandora.print_img + '"></img>');
     $('#print_btn').css("cursor","pointer");
-    console.log(Drupal.settings.basePath + Drupal.settings.islandora.print_link);
     $('#print_btn').click(function() {
       window.location=Drupal.settings.basePath + Drupal.settings.islandora.print_link;
     });
