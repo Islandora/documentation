cd439daab5a69226e8296c8e04c349d585215059
qadan <daitken@discoverygarden.ca>
2013-10-21 14:49:46 -0300

    brrrrrr

--- tests/scripts/travis_setup.sh
+++ tests/scripts/travis_setup.sh
@@ -8,8 +8,8 @@ cd $HOME
 git clone git://github.com/Islandora/tuque.git
 git clone -b $FEDORA_VERSION git://github.com/qadan/islandora_tomcat.git
 cd islandora_tomcat
-export CATALINA_HOME='.'
-export FEDORA_HOME='/home/travis/islandora_tomcat/fedora'
+export CATALINA_HOME=`pwd`
+export FEDORA_HOME='$CATALINA_HOME/fedora'
 ./bin/startup.sh
 cd $HOME
 pear upgrade --force Console_Getopt
