e8390a99eec7fd6e6ef5cab5a7deb993746729ca
Adam Vessey <adam@discoverygarden.ca>
2014-04-22 14:32:42 +0000

    Ensure we can write to the settings.php

--- tests/scripts/travis_setup.sh
+++ tests/scripts/travis_setup.sh
@@ -36,7 +36,9 @@ drush dl --yes drupal
 cd drupal-*
 drush si minimal --db-url=mysql://drupal:drupal@localhost/drupal --yes
 # Needs to make things from Composer be available (PHP CS, primarily)
+sudo chmod a+w sites/default/settings.php
 echo "include_once $HOME/.composer/vendor/autoload.php;" >> sites/default/settings.php
+sudo chmod a-w sites/default/settings.php
 drush runserver --php-cgi=$HOME/.phpenv/shims/php-cgi localhost:8081 &>/dev/null &
 ln -s $ISLANDORA_DIR sites/all/modules/islandora
 mv sites/all/modules/islandora/tests/travis.test_config.ini sites/all/modules/islandora/tests/test_config.ini
