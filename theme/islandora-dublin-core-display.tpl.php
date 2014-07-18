6ca6dc4c068954f970f8dc86d75867c2b11b76cc
Jordan Dukart <jordan@discoverygarden.ca>
2014-07-18 11:55:44 +0000

    Make sure our attribute doesn't break markup.

--- theme/islandora-dublin-core-display.tpl.php
+++ theme/islandora-dublin-core-display.tpl.php
@@ -20,7 +20,7 @@
     <dl xmlns:dcterms="http://purl.org/dc/terms/" class="islandora-inline-metadata islandora-metadata-fields islandora-object-fields">
       <?php $row_field = 0; ?>
       <?php foreach($dc_array as $key => $value): ?>
-        <dt property="<?php print $value['dcterms']; ?>" content="<?php print filter_xss($value['value']); ?>" class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
+        <dt property="<?php print $value['dcterms']; ?>" content="<?php print filter_xss(htmlspecialchars($value['value'], ENT_QUOTES, 'UTF-8')); ?>" class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
           <?php print filter_xss($value['label']); ?>
         </dt>
         <dd class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
