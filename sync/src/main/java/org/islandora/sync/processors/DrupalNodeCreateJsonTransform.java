fe3120a93e57c3158cfdf9396570a9ab019c2900
Daniel Lamb <daniel@discoverygarden.ca>
2015-01-25 16:38:01 -0400

    Initial POC.  Consumes Fedora4 messages and creates drupal nodes.  Camel makes it easy!

---
+++ sync/src/main/java/org/islandora/sync/processors/DrupalNodeCreateJsonTransform.java
@@ -0,0 +1,54 @@
+package org.islandora.sync.processors;
+
+import static org.apache.camel.component.http4.HttpMethods.POST;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class DrupalNodeCreateJsonTransform implements Processor {
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        /*
+         * Make some json that looks like this:
+         * {
+         *   "title":"From Fedora!!!",
+         *   "body":{
+         *     "und":[
+         *       {
+         *          "value": "RDF FROM FEDORA",
+         *          "format":"filtered_html"
+         *       }
+         *     ]
+         *    },
+         *    "type":"article"
+         * }
+         */
+        JSONObject outBody = new JSONObject();
+        outBody.put("title", "From Fedora!!!");
+        outBody.put("type", "article");
+
+        JSONObject bodyField = new JSONObject();
+        JSONObject bodyValue = new JSONObject();
+        JSONArray arr = new JSONArray();
+        bodyValue.put("value", exchange.getIn().getBody(String.class));
+        bodyValue.put("format", "plain_text");
+        arr.add(bodyValue);
+        bodyField.put("und", arr);
+        outBody.put("body", bodyField);
+
+        /* 
+         * Set up the out message to be a POST for the
+         * subsequent call to Drupal's REST service.
+         */
+        Message outMessage = exchange.getOut();
+        outMessage.removeHeaders("*");
+        outMessage.setHeader(Exchange.HTTP_METHOD, POST);
+        outMessage.setHeader(Exchange.CONTENT_TYPE, "application/json");
+        outMessage.setBody(outBody.toJSONString());
+    }
+}
---
+++ sync/src/main/java/org/islandora/sync/routes/DrupalNodeCreate.java
@@ -0,0 +1,20 @@
+package org.islandora.sync.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.fcrepo.camel.JmsHeaders;
+import org.fcrepo.camel.RdfNamespaces;
+import org.islandora.sync.processors.DrupalNodeCreateJsonTransform;
+
+public class DrupalNodeCreate extends RouteBuilder {
+    public void configure() throws Exception {
+
+        from("activemq:topic:fedora")
+            .routeId("fedoraIn")
+            .filter(header(JmsHeaders.EVENT_TYPE).contains(RdfNamespaces.REPOSITORY + "NODE_ADDED"))
+                .to("fcrepo:localhost:8080/fcrepo/rest")
+                .process(new DrupalNodeCreateJsonTransform())
+                .to("http4:localhost/drupal7/rest/node")
+                .log("RESPONSE: ${headers} / ${body}")
+                .to("mock:result");
+    }
+}
