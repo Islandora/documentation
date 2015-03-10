533c2f9f1d57c7caa9618e2a18b5fef58c94588d
Daniel Lamb <daniel@discoverygarden.ca>
2015-03-10 15:41:33 -0300

    Reworking vim.sh.  Had permissions issues.

--- install/vim.sh
+++ install/vim.sh
@@ -2,17 +2,19 @@ HOME_DIR=$1
 
 # Get vundle
 git clone https://github.com/gmarik/Vundle.vim.git "$HOME_DIR/.vim/bundle/Vundle.vim"
+chown -R vagrant:vagrant "$HOME_DIR/.vim"
 
 # Copy over the vimrc
-cp "$HOME_DIR/islandora/install/.vimrc" "$HOME_DIR/.vimrc"
+sudo -u vagrant cp "$HOME_DIR/islandora/install/.vimrc" "$HOME_DIR/.vimrc"
+chown vagrant:vagrant "$HOME_DIR/.vimrc"
 
-# Install plugins using vundle 
-vim -c "PluginInstall" -c "qa"
+# Install plugins using vundle as the vagrant user
+sudo -u vagrant vim -c "PluginInstall" -c "qa"
 
 # Compile YouCompleteMe
 apt-get -y install build-essential cmake python-dev
 cd "$HOME_DIR/.vim/bundle/YouCompleteMe"
-./install.sh
+sudo -u vagrant ./install.sh
 
 # Deps for eclipse (basically... X)
 apt-get -y install xvfb
@@ -21,68 +23,65 @@ apt-get -y install xvfb
 cd $HOME_DIR
 wget -O eclipse.tar.gz http://eclipse.mirror.rafal.ca/technology/epp/downloads/release/luna/SR1a/eclipse-java-luna-SR1a-linux-gtk-x86_64.tar.gz
 tar zxvf eclipse.tar.gz
+chown -R vagrant:vagrant "$HOME_DIR/eclipse"
 
 # Start X (run as the vagrant user)
 sudo -u vagrant Xvfb :1 -screen 0 1024x768x24 &
 
 # Install dependencies for installer
-DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
+sudo -u vagrant DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
   -application org.eclipse.equinox.p2.director \
   -repository http://download.eclipse.org/releases/luna/ \
   -installIU org.eclipse.cdt.feature.group 
 
-DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
+sudo -u vagrant DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
   -application org.eclipse.equinox.p2.director \
   -repository http://download.eclipse.org/releases/luna/ \
   -installIU org.eclipse.dltk.core.feature.group 
 
-DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
+sudo -u vagrant DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
   -application org.eclipse.equinox.p2.director \
   -repository http://download.eclipse.org/releases/luna/ \
   -installIU org.eclipse.dltk.ruby.feature.group 
 
-DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
+sudo -u vagrant DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
   -application org.eclipse.equinox.p2.director \
   -repository http://download.eclipse.org/releases/luna/ \
   -installIU org.eclipse.jdt.feature.group 
 
-DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
+sudo -u vagrant DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
   -application org.eclipse.equinox.p2.director \
   -repository http://download.eclipse.org/releases/luna/ \
   -installIU org.eclipse.php.feature.group 
 
-DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
+sudo -u vagrant DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
   -application org.eclipse.equinox.p2.director \
   -repository http://download.eclipse.org/releases/luna/ \
   -installIU org.eclipse.wst.web_ui.feature.feature.group 
 
-DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
+sudo -u vagrant DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
   -application org.eclipse.equinox.p2.director \
   -repository https://dl-ssl.google.com/android/eclipse/ \
   -installIU com.android.ide.eclipse.adt.feature.feature.group 
 
-DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
+sudo -u vagrant DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
   -application org.eclipse.equinox.p2.director \
   -repository http://dist.springsource.org/release/GRECLIPSE/e4.4/ \
   -installIU org.codehaus.groovy.eclipse.feature.feature.group 
 
-DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
+sudo -u vagrant DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
   -application org.eclipse.equinox.p2.director \
   -repository http://pydev.org/updates \
   -installIU org.python.pydev.feature.feature.group 
 
-DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
+sudo -u vagrant DISPLAY=:1 "$HOME_DIR/eclipse/eclipse" -nosplash -consolelog -debug \
   -application org.eclipse.equinox.p2.director \
   -repository http://download.scala-ide.org/sdk/lithium/e44/scala211/dev/site \
   -installIU org.scala-ide.sdt.feature.feature.group 
 
 # Get Eclim
 git clone git://github.com/ervandew/eclim.git
-
-# chown things back to the vagrant user before installing eclim
-chown -R vagrant:vagrant "$HOME_DIR/eclipse"
 chown -R vagrant:vagrant "$HOME_DIR/eclim"
-chown -R vagrant:vagrant "$HOME_DIR/.vim"
 
 # Build and install from source (run as the vagrant user)
 cd eclim
@@ -90,5 +89,3 @@ sudo -u vagrant ant -Declipse.home="$HOME_DIR/eclipse" -Dvim.files="$HOME_DIR/.v
 
 # Start Eclim (run as the vagrant user)
 sudo -u vagrant DISPLAY=:1 "$HOME_DIR/eclipse/eclimd" -b
-
-# Wait a few minutes for all the crazy output to stop... and enjoy!
