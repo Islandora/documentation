c1585ea49560c91f79e97bdcc840f9eaa4e9ae86
Daniel Lamb <daniel@discoverygarden.ca>
2015-07-14 11:47:37 -0300

    Chowning vagrant users maven repo

--- install/services.sh
+++ install/services.sh
@@ -2,6 +2,9 @@ echo "Installing Islandora Services"
 
 HOME_DIR=$1
 
+# Chown everything over to the vagrant user just in case
+chown -R vagrant:vagrant $HOME_DIR/.m2
+
 cd "$HOME_DIR"/islandora/camel/services
 
 cd collection-service
--- install/sync.sh
+++ install/sync.sh
@@ -2,6 +2,9 @@ echo "Installing Sync."
 
 HOME_DIR=$1
 
+# Chown everything over to the vagrant user just in case
+chown -R vagrant:vagrant $HOME_DIR/.m2
+
 cd "$HOME_DIR/islandora/camel/sync"
 
 sudo -u vagrant mvn install
