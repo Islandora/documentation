7cfdcfb9163dbf43b2189b004ea23e7a1782c868
Daniel Lamb <daniel@discoverygarden.ca>
2015-07-15 13:48:51 +0000

    Cycling karaf at the end of the install script for good measure.  Should get rid of the need for all the sleeps.

--- install/post-install.sh
+++ install/post-install.sh
@@ -14,8 +14,9 @@ chmod -R g+w /var/www/html
 # Chown the home directory for good measure
 chown -R vagrant:vagrant "$HOME_DIR"
 
-# Just for good measure
+# Also just for good measure
 service tomcat7 restart
+service karaf-service restart
 
 # Fix ApacheSolr config
 drush -r /var/www/html/drupal sqlq "update apachesolr_environment set url='http://localhost:8080/solr' where url='http://localhost:8983/solr'"
