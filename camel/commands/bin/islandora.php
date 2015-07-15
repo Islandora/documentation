d0e9cc2ee3d44c93d72d999ad656b2a14d9e3531
Daniel Lamb <daniel@discoverygarden.ca>
2015-07-15 13:46:21 +0000

    Some comments on that missing file.

--- camel/commands/bin/islandora.php
+++ camel/commands/bin/islandora.php
@@ -13,18 +13,22 @@ use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\Finder\Finder;
 
 $dispatcher = new EventDispatcher();
+
+// Set the error handler to wrap errors as exceptions at the beginning of
+// every command.
 $dispatcher->addListener(ConsoleEvents::COMMAND, function (ConsoleCommandEvent $event) {
     set_error_handler(function (int $errno, string $errstr, string $errfile, int $errline, array $errcontext) {
         throw new ErrorException($errstr, 1, $errno, $errfile, $errline);
     });
 });
+
+// Write output to stderr (if available) when there's an exception during
+// command execution.
 $dispatcher->addListener(ConsoleEvents::EXCEPTION, function (ConsoleExceptionEvent $event) {
-    // Default $stdErr variable to output
     $std_err = $event->getOutput();
 
     if ($std_err instanceof ConsoleOutputInterface)
     {
-        // If it's available, get stdErr output
         $std_err = $std_err->getErrorOutput();
     }
 
@@ -35,6 +39,8 @@ $dispatcher->addListener(ConsoleEvents::EXCEPTION, function (ConsoleExceptionEve
 $application = new Application();
 $application->setDispatcher($dispatcher);
 
+// A little magic to find all IslandoraCommand classes and dynamically
+// instantiate them for the Symfony Console Application.
 $finder = new Finder();
 $iter = new ClassIterator($finder->in('../src'));
 $iter->enableAutoloading();
@@ -44,4 +50,5 @@ foreach ($iter->type('Islandora\IslandoraCommand')->where('isInstantiable') as $
     $application->add(new $class_name());
 }
 
+// Run the command
 $application->run();
