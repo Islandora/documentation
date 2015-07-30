ce1d1e8f0fd18275591c11cf1f46746e9722ae9b
Daniel Lamb <daniel@discoverygarden.ca>
2015-07-30 16:33:47 -0300

    Patching up build script

--- install/drupal.sh
+++ install/drupal.sh
@@ -88,6 +88,8 @@ drush -y en islandora_basic_image
 drush -y en islandora_collection
 drush -y en islandora_apachesolr
 drush -y en islandora_delete_by_fedora_uri
+drush -y en islandora_medium_size_service
+drush -y en islandora_tn_service
 
 # Coder & Code Sniffer
 pear install PHP_CodeSniffer
