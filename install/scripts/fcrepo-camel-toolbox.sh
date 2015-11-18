75cbdc27f6bbc2b68f37626a74af887120daae9b
nruest <ruestn@gmail.com>
2015-11-18 05:53:28 -0500

    Use fcrepo-camel-toolbox in Karaf.

--- install/scripts/fcrepo-camel-toolbox.sh
+++ install/scripts/fcrepo-camel-toolbox.sh
@@ -3,18 +3,23 @@ echo "Installing Fcrepo-Camel-Toolbox"
 
 HOME_DIR=$1
 
+cd $HOME_DIR
+
 if [ -f "$HOME_DIR/islandora/install/configs/variables" ]; then
   . "$HOME_DIR"/islandora/install/configs/variables
 fi
 
-if [ ! -f "$DOWNLOAD_DIR/fcrepo-camel-toolbox.war" ]; then
+if [ ! -f "$DOWNLOAD_DIR/fcrepo-camel-toolbox.tar.gz" ]; then
   echo "Downloading fcrepo-camel-toolbox"
-  wget -O "$DOWNLOAD_DIR/fcrepo-camel-toolbox.war" "https://github.com/fcrepo4-labs/fcrepo-camel-toolbox/releases/download/fcrepo-camel-toolbox-$FCREPO_CAMEL_VERSION/fcrepo-camel-webapp-at-is-it-$FCREPO_CAMEL_VERSION.war"
+  wget -O "$DOWNLOAD_DIR/fcrepo-camel-toolbox.tar.gz" https://github.com/fcrepo4-exts/fcrepo-camel-toolbox/archive/fcrepo-camel-toolbox-"$FCREPO_CAMEL_VERSION".tar.gz
 fi
 
-cd /var/lib/tomcat7/webapps
-cp -v "$DOWNLOAD_DIR/fcrepo-camel-toolbox.war" "/var/lib/tomcat7/webapps"
-chown tomcat7:tomcat7 /var/lib/tomcat7/webapps/fcrepo-camel-toolbox.war
+cd "$DOWNLOAD_DIR"
+tar -xzvf fcrepo-camel-toolbox.tar.gz
+cd fcrepo-camel-toolbox
+MAVEN_OPTS="-Xmx1024m" sudo -u vagrant mvn install
+
+"$KARAF_CLIENT" < "$KARAF_CONFIGS/fcrepo-camel-toolbox.script"
 
 if [ $(grep -c '\-Dtriplestore.baseUrl=' /etc/default/tomcat7) -eq 0 ]; then
   echo "JAVA_OPTS=\"\$JAVA_OPTS -Dtriplestore.baseUrl=localhost:8080/bigdata/sparql\"" >> /etc/default/tomcat7
