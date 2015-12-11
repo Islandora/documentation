d3d34c838b537d94345dc9998923d794d5576813
Daniel Lamb <daniel@discoverygarden.ca>
2015-12-11 09:29:23 -0400

    Using view handler to convert PSR-7 to Symfony responses.  Using the FedoraApi class from Chullo.

--- services/TransactionService/src/index.php
+++ services/TransactionService/src/index.php
@@ -5,9 +5,10 @@ namespace Islandora\TransactionService;
 require_once __DIR__.'/../vendor/autoload.php';
 
 use GuzzleHttp\Exception\ClientException;
-use Islandora\Chullo\Chullo;
+use Islandora\Chullo\FedoraApi;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Response;
+use Psr\Http\Message\ResponseInterface;
 
 date_default_timezone_set('UTC');
 
@@ -16,61 +17,41 @@ $app = new Application();
 $app['debug'] = true;
 
 $app['fedora'] = function () use ($app) {
-    return Chullo::create('http://127.0.0.1:8080/fcrepo/rest');
+    return FedoraApi::create('http://127.0.0.1:8080/fcrepo/rest');
 };
+
+/**
+ * Convert returned Guzzle responses to Symfony responses.
+ */
+$app->view(function (ResponseInterface $psr7) {
+    return new Response($psr7->getBody(), $psr7->getStatusCode(), $psr7->getHeaders());
+});
 
 $app->post(
     "/islandora/transaction",
     function (Application $app) {
-        try {
-            return new Response($app['fedora']->createTransaction(), 201);
-        }
-        catch (ClientException $e) {
-            $response = $e->getResponse();
-            $app->abort($response->getStatusCode(), $response->getReasonPhrase());
-        }
+        return $app['fedora']->createTransaction();
     }
 );
 
 $app->post(
     "/islandora/transaction/{id}/extend",
     function (Application $app, $id) {
-        try {
-            $app['fedora']->extendTransaction($id);
-            return new Response("", 204);
-        }
-        catch (ClientException $e) {
-            $response = $e->getResponse();
-            $app->abort($response->getStatusCode(), $response->getReasonPhrase());
-        }
+        return $app['fedora']->extendTransaction($id);
     }
 );
 
 $app->post(
     "/islandora/transaction/{id}/commit",
     function (Application $app, $id) {
-        try {
-            $app['fedora']->commitTransaction($id);
-            return new Response("", 204);
-        }
-        catch (ClientException $e) {
-            $response = $e->getResponse();
-            $app->abort($response->getStatusCode(), $response->getReasonPhrase());
-        }
+        return $app['fedora']->commitTransaction($id);
     }
 );
 
 $app->post(
     "/islandora/transaction/{id}/rollback",
     function (Application $app, $id) {
-        try {
-            return $app['fedora']->rollbackTransaction($id);
-            return new Response("", 204);
-        }
-        catch (ClientException $e) {
-            $response = $e->getResponse();
-            $app->abort($response->getStatusCode(), $response->getReasonPhrase());
-        }
+        return $app['fedora']->rollbackTransaction($id);
     }
 );
 
