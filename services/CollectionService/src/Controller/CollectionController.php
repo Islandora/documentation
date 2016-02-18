97a5bcfc644fa2ad6997f04a2e79b7afaf4b0e47
Daniel Lamb <daniel@discoverygarden.ca>
2016-02-18 17:19:39 -0400

    Temp commit before switching branches.

---
+++ services/CollectionService/src/Controller/CollectionController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Islandora\CollectionService\Controller;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CollectionController {
+
+}
