13ff22d90e6147cca0ac3d7550872a3cf79eb1a2
Jared Whiklo <jwhiklo@gmail.com>
2016-03-22 20:25:05 -0500

    Fixed the Apache Virtual host setup.
    Included a script to align the git branches required by the various composer.json files

---
+++ install/scripts/align_branches.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# whikloj - 2016-03-22
+#
+# Changes the version for islandora/resource-service or
+# islandora/transaction-service to 'dev-<current branch>'
+
+# Currently selected branch in git
+CURRENT_BRANCH=$(git branch | grep '*' | cut -d' ' -f2)
+# List of directories to change composer.json in 
+AFFECTS_DIR="CollectionService TransactionService"
+
+echo -e "Altering composer.json to use branch 'dev-${CURRENT_BRANCH}'..."
+for i in $AFFECTS_DIR; do
+  mv $i/composer.json $i/composer.json_bkup
+  awk -v BRANCH=$CURRENT_BRANCH '/islandora\/(resource|transaction)/ { print gensub(/(.+):\s*"dev-[^"]+/, "\\1: \"dev-"BRANCH, 1)} !/islandora\/(resource|transaction)/ { print $0}' $i/composer.json_bkup > $i/composer.json
+done
+echo "done"
+
--- install/scripts/islandora-microservices.sh
+++ install/scripts/islandora-microservices.sh
@@ -17,7 +17,10 @@ fi
 
 /etc/init.d/apache2 restart
 
+cp "$HOME_DIR/islandora/install/scripts/align_branches.sh" "/opt/islandora/services"
 cd /opt/islandora/services
+./align_branches.sh
+
 php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
 php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '41e71d86b40f28e771d4bb662b997f79625196afcca95a5abf44391188c695c6c1456e16154c75a211d238cc3bc5cb47') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
