a7b1fc6baf528557711c7258130da3d491921280
Benjamin Rosner <brosner@barnard.edu>
2016-04-25 12:51:49 -0400

    First pass at addressing #150.

--- services/ResourceService/src/Controller/ResourceController.php
+++ services/ResourceService/src/Controller/ResourceController.php
@@ -68,17 +68,38 @@ class ResourceController {
   public function delete(Application $app, Request $request, $id, $child) {
     $tx = $request->query->get('tx', "");
     $force = $request->query->get('force', FALSE);
+      
+    error_log('---START OF DELETE RESOURCE REQUEST: ');
+    error_log($id);
+
+    $delete_queue = array($app->escape($id).'/'.$child, );
+    $sparql_query = $app['twig']->render('findAllOreProxy.sparql', array(
+       'resource' => $id,
+    ));
     try {
-      $response = $app['api']->deleteResource($app->escape($id).'/'.$child, $tx);
-      //remove tombstone also if 'force' == true and previous response is 204
-      if ((204 == $response->getStatusCode() || 410 == $response->getStatusCode()) && $force) {
-        $response= $app['api']->deleteResource($app->escape($id).'/'.$child.'/fcr:tombstone', $tx);
-      }
+      $sparql_result = $app['triplestore']->query($sparql_query);
     }
     catch (\Exception $e) {
-      $app->abort(503, '"Chullo says Fedora4 Repository is Not available"');
+      $app->abort(503, 'Chullo says "Triple Store Not available"');
     }
-    return $response;
+    if (count($sparql_result) > 0) {
+      foreach ($sparql_result as $ore_proxy) {
+        $delete_queue[] = $ore_proxy->obj->getUri();
+      }
+    }
+    error_log(implode(',', $delete_queue));
+    foreach($delete_queue as $object_uri) {
+      try {
+        $response = $app['api']->deleteResource($object_uri, $tx);
+        //remove tombstone also if 'force' == true and previous response is 204
+        if ((204 == $response->getStatusCode() || 410 == $response->getStatusCode()) && $force) {
+          $response = $app['api']->deleteResource($object_uri.'/fcr:tombstone', $tx);
+        }
+      }
+      catch (\Exception $e) {
+        $app->abort(503, '"Chullo says Fedora4 Repository is Not available"');
+      }
+      error_log($response->getStatusCode());
+    }
   }
-  
 }
