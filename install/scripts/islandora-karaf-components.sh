f5a9faa8a9e0ab6422c076481127343bc2eb7a8f
nruest <ruestn@gmail.com>
2016-04-27 13:35:54 -0400

    Code review.

--- install/scripts/islandora-karaf-components.sh
+++ install/scripts/islandora-karaf-components.sh
@@ -10,11 +10,5 @@ fi
 echo "Installing Islandora Sync Gateway"
 $KARAF_CLIENT -f $KARAF_CONFIGS/islandora_sync_gateway.script
 sleep 10
-echo "Installing Islandora Basic Image Service"
-$KARAF_CLIENT -f $KARAF_CONFIGS/islandora_basic_image.script
-sleep 10
-echo "Installing Islandora Collection Service"
-$KARAF_CLIENT -f $KARAF_CONFIGS/islandora_collection.script
-sleep 10
 echo "Installing Islandora Triplestore Indexer"
 $KARAF_CLIENT -f $KARAF_CONFIGS/islandora_indexing_triplestore.script
