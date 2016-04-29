42ffa8aa69446454d223d851e31e9d7f023c78ce
nruest <ruestn@gmail.com>
2016-04-29 08:45:43 -0400

    I think this is it.

--- install/scripts/lamp-server.sh
+++ install/scripts/lamp-server.sh
@@ -2,7 +2,7 @@
 
 echo "Installing LAMP server packages"
 
-PACKAGES="libwrap0 ssl-cert libterm-readkey-perl mysql-client-5.5 libdbi-perl libmysqlclient18 mysql-server-core-5.5 mysql-common apache2 mysql-server-5.5 mysql-client-core-5.5 tcpd libaio1 mysql-server libdbd-mysql-perl libhtml-template-perl php5"
+PACKAGES="libwrap0 ssl-cert libterm-readkey-perl mysql-client-5.5 libdbi-perl libmysqlclient18 mysql-server-core-5.5 mysql-common apache2 mysql-server-5.5 mysql-client-core-5.5 tcpd libaio1 mysql-server libdbd-mysql-perl libhtml-template-perl php5.6"
 
 apt-get -qq install -y $PACKAGES
 
