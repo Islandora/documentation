c1a91b823a106f116aea14fe6df5927f08238bad
nruest <ruestn@gmail.com>
2016-05-24 22:37:11 -0400

    Forget about Alpaca.

--- install/scripts/alpaca.sh
+++ install/scripts/alpaca.sh
@@ -8,14 +8,11 @@ if [ -f "$HOME_DIR/islandora/install/configs/variables" ]; then
   . "$HOME_DIR"/islandora/install/configs/variables
 fi
 
-# Chown everything over to the vagrant user just in case
-chown -R vagrant:vagrant "$HOME_DIR/.m2"
-
 cd "$HOME_DIR"
 git clone https://github.com/Islandora-CLAW/Alpaca.git
 cd Alpaca
-
-mvn -q install
+chown -R vagrant:vagrant "$HOME_DIR/Alpaca"
+sudo -u vagrant mvn -q install
 
 # Chown everything over to the vagrant user just in case
 chown -R vagrant:vagrant "$HOME_DIR/.m2"
