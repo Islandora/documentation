19d73b430ba5664fd32f98e2ceb03c65cf60fa2c
Daniel Lamb <dlamb@islandora.ca>
2016-10-07 12:44:37 -0300

    Adapting install for pruned alpaca

--- install/scripts/islandora-karaf-components.sh
+++ install/scripts/islandora-karaf-components.sh
@@ -7,9 +7,6 @@ if [ -f "$HOME_DIR/islandora/install/configs/variables" ]; then
   . "$HOME_DIR"/islandora/install/configs/variables
 fi
 
-echo "Installing Islandora Sync Gateway"
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-$KARAF_CLIENT -f $KARAF_CONFIGS/islandora_sync_gateway.script
-sleep 10
 echo "Installing Islandora Triplestore Indexer"
 $KARAF_CLIENT -f $KARAF_CONFIGS/islandora_indexing_triplestore.script
