c5fb226aea907a8f3aaf276cdba0e9ac2dd81214
Daniel Lamb <dlamb@islandora.ca>
2016-10-12 14:59:51 -0300

    Making the alpaca install point to the already cloned CLAW repo.

--- install/scripts/alpaca.sh
+++ install/scripts/alpaca.sh
@@ -9,11 +9,4 @@ if [ -f "$HOME_DIR/islandora/install/configs/variables" ]; then
 fi
 
 cd "$HOME_DIR"/islandora/Alpaca
-#cd "$HOME_DIR"
-#git clone https://github.com/Islandora-CLAW/Alpaca.git
-#cd Alpaca
-#chown -R vagrant:vagrant "$HOME_DIR/Alpaca"
 sudo -u vagrant ./gradlew build
-
-# Chown everything over to the vagrant user just in case
-#chown -R vagrant:vagrant "$HOME_DIR/Alpaca"
