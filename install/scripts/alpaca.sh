8d705adf1f131f1e6670b76de9408b4903d6da3d
Daniel Lamb <dlamb@islandora.ca>
2016-10-21 11:36:34 -0300

    Putting it back to point to head....

--- install/scripts/alpaca.sh
+++ install/scripts/alpaca.sh
@@ -8,5 +8,11 @@ if [ -f "$HOME_DIR/islandora/install/configs/variables" ]; then
   . "$HOME_DIR"/islandora/install/configs/variables
 fi
 
-cd "$HOME_DIR"/islandora/Alpaca
+cd "$HOME_DIR"
+git clone https://github.com/Islandora-CLAW/Alpaca.git
+cd Alpaca
+chown -R vagrant:vagrant "$HOME_DIR/Alpaca"
 sudo -u vagrant ./gradlew build
+
+# Chown everything over to the vagrant user just in case
+chown -R vagrant:vagrant "$HOME_DIR/Alpaca"
