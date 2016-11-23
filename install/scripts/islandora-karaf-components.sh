ee6573b3f179e58e8f05a45998a63187d0a19f75
Daniel Lamb <dlamb@islandora.ca>
2016-11-23 12:45:35 -0400

    Id mapping service install

--- install/scripts/islandora-karaf-components.sh
+++ install/scripts/islandora-karaf-components.sh
@@ -8,5 +8,19 @@ if [ -f "$HOME_DIR/islandora/install/configs/variables" ]; then
 fi
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+
+echo "Bootstrapping ID Mapping Database"
+mysql -u root -pislandora -e 'CREATE DATABASE idiomatic;'
+mysql -u root -pislandora -e 'CREATE TABLE idiomatic.uris ( public VARCHAR(1024), fedora VARCHAR(1024) NOT NULL, PRIMARY KEY (public)  );'
+
+echo "Installing mysql-connector"
+cd "$HOME_DIR"
+wget http://downloads.mysql.com/archives/get/file/mysql-connector-java-6.0.4.tar.gz
+tar zxvf mysql-connector-java-6.0.4.tar.gz
+cp mysql-connector-java-6.0.4/mysql-connector-java-6.0.4-bin.jar "$KARAF_DIR"/deploy
+
+echo "Installing ID Mapping Service"
+$KARAF_CLIENT -f $KARAF_CONFIGS/islandora_id_mapping_service.script
+
 echo "Installing Islandora Triplestore Indexer"
 $KARAF_CLIENT -f $KARAF_CONFIGS/islandora_indexing_triplestore.script
