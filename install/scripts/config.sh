fda0b6db9a14f22b08de3a458c2bae1c6dc48e20
Daniel Lamb <dlamb@islandora.ca>
2016-11-24 12:46:00 -0400

    changes from testing

--- install/scripts/config.sh
+++ install/scripts/config.sh
@@ -22,14 +22,14 @@ fi
 if [ -f "$KARAF_DIR/etc/org.ops4j.datasource-idiomatic.cfg" ]; then
   # Update fcrepo triplestore indexing config
   sed -i 's|user=|user=root|' "$KARAF_DIR/etc/org.ops4j.datasource-idiomatic.cfg"
-  sed -i 's|pass=|user=islandora|' "$KARAF_DIR/etc/org.ops4j.datasource-idiomatic.cfg"
+  sed -i 's|password=|password=islandora|' "$KARAF_DIR/etc/org.ops4j.datasource-idiomatic.cfg"
 else
   echo "$KARAF_DIR/etc/org.ops4j.datasource-idiomatic.cfg still doesn't exist, this is an ERROR!"
 fi
 
 if [ -f "$KARAF_DIR/etc/edu.amherst.acdc.connector.broadcast.cfg" ]; then
   # Update fcrepo triplestore indexing config
-  sed -i 's|message.recipients=|message.recipients=activemq:queue:acrepo-connector-idiomatic,activemq:queue:islandora/indexing/triplestore|' "$KARAF_DIR/etc/edu.amherst.acdc.connector.broadcast.cfg"
+  sed -i 's|message.recipients=|message.recipients=activemq:queue:acrepo-connector-idiomatic|' "$KARAF_DIR/etc/edu.amherst.acdc.connector.broadcast.cfg"
 else
   echo "$KARAF_DIR/etc/edu.amherst.acdc.connector.broadcast.cfg still doesn't exist, this is an ERROR!"
 fi
@@ -37,8 +37,8 @@ fi
 if [ -f "$KARAF_DIR/etc/edu.amherst.acdc.connector.idiomatic.cfg" ]; then
   # Update fcrepo triplestore indexing config
   sed -i 's|input.stream=broker:topic:fedora|input.stream=activemq:queue:acrepo-connector-idiomatic|' "$KARAF_DIR/etc/edu.amherst.acdc.connector.idiomatic.cfg"
-  sed -i 's|id.property=dc:identifier|id.property=schema:url|' "$KARAF_DIR/etc/edu.amherst.acdc.connector.idiomatic.cfg"
-  sed -i 's|id.namespace=http://purl.org/dc/elements/1.1/|id.namespace=http://schema.org/|' "$KARAF_DIR/etc/edu.amherst.acdc.connector.idiomatic.cfg"
+  sed -i 's|id.property=dc:identifier|id.property=nfo:uuid|' "$KARAF_DIR/etc/edu.amherst.acdc.connector.idiomatic.cfg"
+  sed -i 's|id.namespace=http://purl.org/dc/elements/1.1/|id.namespace=http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#|' "$KARAF_DIR/etc/edu.amherst.acdc.connector.idiomatic.cfg"
 else
   echo "$KARAF_DIR/etc/edu.amherst.acdc.connector.idiomatic.cfg still doesn't exist, this is an ERROR!"
 fi
--- install/scripts/islandora-karaf-components.sh
+++ install/scripts/islandora-karaf-components.sh
@@ -10,14 +10,14 @@ fi
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 echo "Bootstrapping ID Mapping Database"
-mysql -u root -pislandora -e 'CREATE DATABASE idiomatic;'
-mysql -u root -pislandora -e 'CREATE TABLE idiomatic.uris ( public VARCHAR(1024), fedora VARCHAR(1024) NOT NULL, PRIMARY KEY (public)  );'
+mysql -u "$MYSQL_USER" -p"$MYSQL_PASS" -e 'CREATE DATABASE idiomatic;'
+mysql -u "$MYSQL_USER" -p"$MYSQL_PASS" -e 'CREATE TABLE idiomatic.uris ( public VARCHAR(1024), fedora VARCHAR(1024) NOT NULL, PRIMARY KEY (public)  );'
 
 echo "Installing mysql-connector"
 cd "$HOME_DIR"
-wget http://downloads.mysql.com/archives/get/file/mysql-connector-java-6.0.4.tar.gz
-tar zxvf mysql-connector-java-6.0.4.tar.gz
-cp mysql-connector-java-6.0.4/mysql-connector-java-6.0.4-bin.jar "$KARAF_DIR"/deploy
+wget http://downloads.mysql.com/archives/get/file/mysql-connector-java-"$MYSQL_CONNECTOR_VERSION".tar.gz
+tar zxvf mysql-connector-java-"$MYSQL_CONNECTOR_VERSION".tar.gz
+cp mysql-connector-java-"$MYSQL_CONNECTOR_VERSION"/mysql-connector-java-"$MYSQL_CONNECTOR_VERSION"-bin.jar "$KARAF_DIR"/deploy
 
 echo "Installing ID Mapping Service"
 $KARAF_CLIENT -f $KARAF_CONFIGS/islandora_id_mapping_service.script
