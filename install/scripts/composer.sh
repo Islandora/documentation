0289af3526248c7237d60600d5c25532cbcc1ba6
Daniel Lamb <dlamb@islandora.ca>
2017-01-13 15:06:28 -0400

    Adding github token to composer

--- install/scripts/composer.sh
+++ install/scripts/composer.sh
@@ -4,3 +4,4 @@ echo "Installing Composer"
 curl -sS https://getcomposer.org/installer | php
 php composer.phar install --no-progress
 mv composer.phar /usr/local/bin/composer
+composer config --global github-oauth.github.com $GITHUB_TOKEN
