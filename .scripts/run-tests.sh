04f83cec0ef2800346fa405bab6717db9d1980d4
dannylamb <dlamb@islandora.ca>
2020-11-25 14:47:25 -0400

    Removing scripts (#1672)

--- .scripts/run-tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Wrapper for the test executing function so we only have to change it in one place.
-# The module name gets passed in as a command line arg.
-php core/scripts/run-tests.sh --suppress-deprecations --url http://127.0.0.1:8282 --verbose --php `which php` --module "$1"
